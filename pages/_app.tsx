8a1cf33b21b395c2e0de7a6268d96bf1492f03b4
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-08-14 13:06:19 -0600

    creating body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import '../styles/globals.css'
+import {useState, useEffect} from 'react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Navbar from '../components/Navbar'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect( () => {
+    setIsSSR(false)
+  },[])
+
+  if(isSSR) return null;
+
+  return(
+    <div>
+      <Navbar />
+      {/* flex gap-6 md:gap-20 */}
+      <div>
+        {/* h-92vh overflox-hidden xl hover:overflow-auto */}
+        <div>
+          Sidebar 
+        </div>  
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
